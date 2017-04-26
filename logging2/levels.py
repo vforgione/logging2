088471715ef61c1aef949c6a6c04419ee8a624f5
Vince Forgione <v.forgione@gmail.com>
2017-04-26 16:21:33 -0500

    Added `levels` Module
    - added `levels.py`
    - added tests
    - created `setup.py` for testing
    - created `setup.cfg` to control testing

---
+++ logging2/levels.py
@@ -0,0 +1,60 @@
+from enum import Enum
+
+
+class LogLevel(Enum):
+    """A workflow control construct that enables setting a minimum level of
+    verbosity for log entries.
+    """
+
+    debug: int = 0
+    info: int = 1
+    warning: int = 2
+    error: int = 3
+    exception: int = 4
+
+    def __str__(self) -> str:
+        """Stringifies the ``name`` of the ``LogLevel`` enum
+
+        :returns: the name of the level in upper case
+        """
+        return self.name.upper()
+
+    def __lt__(self, other: 'LogLevel') -> bool:
+        """Checks if this ``LogLevel`` is less than another
+
+        :param other: the other level to test against
+        :returns: is this level is less than the other
+        """
+        return self.value < other.value
+
+    def __le__(self, other: 'LogLevel') -> bool:
+        """Checks if this ``LogLevel`` is less than or equal to another
+
+        :param other: the other level to test against
+        :returns: is this level is less than or equal to the other
+        """
+        return self.value <= other.value
+
+    def __eq__(self, other: 'LogLevel') -> bool:
+        """Checks if this ``LogLevel`` is equal to another
+
+        :param other: the other level to test against
+        :returns: is this level is equal to the other
+        """
+        return self.value == other.value
+
+    def __ge__(self, other: 'LogLevel') -> bool:
+        """Checks if this ``LogLevel`` is greater than or equal to another
+
+        :param other: the other level to test against
+        :returns: is this level is greater than or equal to the other
+        """
+        return self.value >= other.value
+
+    def __gt__(self, other: 'LogLevel') -> bool:
+        """Checks if this ``LogLevel`` is greater than another
+
+        :param other: the other level to test against
+        :returns: is this level is greater than the other
+        """
+        return self.value > other.value
---
+++ setup.py
@@ -0,0 +1,47 @@
+import codecs
+import os
+from setuptools import setup
+
+
+# semantic versioning
+VERSION = '0.1.0'
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, 'README.rst'), encoding='utf8') as fh:
+    long_description = fh.read()
+
+with codecs.open(os.path.join(here, 'requirements.txt'), encoding='utf8') as fh:
+    dev_reqs = [line for line in fh]
+
+
+setup(
+    name='logging2',
+    version=VERSION,
+    description='A More Pythonic Logging System',
+    long_description=long_description,
+    url='https://github.com/vforgione/logging2',
+    author='Vince Forgione',
+    author_email='v.forgione@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'Natural Language :: English',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='logging',
+    packages=['logging2'],
+    extras_require={
+        'dev': dev_reqs,
+    }
+)
